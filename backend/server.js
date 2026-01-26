05d64e8fc6d6cae490686b594a122db75092d1df
Jayeshs1999 <jayeshsevatkar55@gmail.com>
2026-01-26 18:10:36 +0530

    added url

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ app.use(
       "http://localhost:3000",
       "http://192.168.0.147:3000",
       "http://localhost:3000",
-      "https://dormitory-backend-5rda.onrender.com",
+      "https://dormitory-alpha.vercel.app/",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
