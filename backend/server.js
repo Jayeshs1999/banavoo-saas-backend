f9f2c6f19af63e82870e64ffeab0c77cbe64c3c8
Jayeshs1999 <jayeshsevatkar55@gmail.com>
2026-02-03 22:38:32 +0530

    added new domain in backend

--- backend/server.js
+++ backend/server.js
@@ -34,6 +34,7 @@ app.use(
       "http://192.168.0.147:3000",
       "http://localhost:3000",
       "https://dormitory-alpha.vercel.app",
+      "https://www.sthals.in",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
